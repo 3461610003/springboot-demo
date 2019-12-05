c1fa059a7f23fa7aa51eb692684c4a6e61f50d56
zhenghao <zh@alicms.com>
2019-12-05 09:29:40 +0800

    还原测试提交1

--- src/main/java/com/alicms/example/demo/service/HelloService.java
+++ src/main/java/com/alicms/example/demo/service/HelloService.java
@@ -1,15 +1,29 @@
 package com.alicms.example.demo.service;
 
-import org.apache.ibatis.annotations.Update;
-
 /**
- * @Description TODO
+ * @Description HelloService
  * @Author zhenghao
  * @Date 2019/9/3 16:48
  */
 public interface HelloService {
 
+    /**
+     * @description: sayHello
+     * @author: zhenghao
+     * @date: 2019/12/5 9:07
+     */
     String sayHello();
+
+    /**
+     * 获取数据库cron表达式
+     * @return cron表达式
+     */
     String getCron();
+
+    /**
+     * 修改cron表达式
+     * @param cron 要修改的cron表达式
+     * @return
+     */
     int updateCron(String cron);
 }
