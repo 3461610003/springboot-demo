bf4865300de752213391ce7e18a601d7e2617ac9
zhenghao <zh@alicms.com>
2019-12-05 09:32:39 +0800

    还原版本重新提交

---
+++ src/main/java/com/alicms/example/demo/service/TestService.java
@@ -0,0 +1,16 @@
+package com.alicms.example.demo.service;
+
+/**
+ * @Description: 测试Service
+ * @Author zhenghao
+ * @Date 2019/12/5 9:12
+ */
+public interface TestService {
+
+    /**
+     * @description: 测试方法
+     * @author: zhenghao
+     * @date: 2019/12/5 9:14
+     */
+    void test();
+}
