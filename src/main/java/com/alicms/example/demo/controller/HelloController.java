345aa18a909c01bf35fd2af59e5184c20aaa8769
zhenghao <zh@alicms.com>
2019-12-05 09:25:16 +0800

    还原测试提交1

--- src/main/java/com/alicms/example/demo/controller/HelloController.java
+++ src/main/java/com/alicms/example/demo/controller/HelloController.java
@@ -16,7 +16,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("hello")
 public class HelloController {
-// 测试提交3.。。。。。。。。。。。。。。。。。。。。。。。。。。。。
+
     @Resource
     private HelloService helloService;
 
