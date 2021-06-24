25aaefef0903d1d52fdecf35abc0f8fd62f3c974
zhenghao <zh@alicms.com>
2021-06-24 16:47:38 +0800

    update

--- src/test/java/com/alicms/example/demo/TempEmailTest.java
+++ src/test/java/com/alicms/example/demo/TempEmailTest.java
@@ -29,8 +29,8 @@ public class TempEmailTest {
         System.out.println("-----------------------------------------------------------");
         System.out.println(email);
         System.out.println("-----------------------------------------------------------");
-//        String code = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImRvbWluaWNseW1hbjYuNC43MDcwMkBnbWFpbC5jb20iLCJ0aW1lc3RhbXAiOjE2MjQ1MDc0NDl9.IEotjY6ijLdpe--kwSqPX4L4BkeaVfEwYHmH-VHLM_M";
-//        String email = "dominiclyman6.4.70702@gmail.com";
+//        String code = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImdyYWNlbWF5NjkuNTUuNzQyQGdtYWlsLmNvbSIsInRpbWVzdGFtcCI6MTYyNDUyMzk3Nn0.UBcqq5UGQDXl0AUPHt9K4xOsLAPj1CAdJ4HFJQMgOf0";
+//        String email = "gracemay69.55.742@gmail.com";
         printEmailMsgCode(code, email);
     }
 
@@ -42,27 +42,27 @@ public class TempEmailTest {
         header.put("x-xsrf-token", X_XSRF_TOKEN);
         String param = "{\"type\":\"random\",\"domains\":[1,2,3,5,6,7,12],\"format\":\"restore\",\"username\":null,\"locale\":\"en_US\",\"minStr\":6,\"maxStr\":15}";
 //        String result = HttpClientUtils.doPost("https://smailpro.com/app/email", param, header);
-       while (true) {
-           String result = HttpClientUtil.doPostJson("https://smailpro.com/app/email", param, header);
-           // {"code":200,"msg":"OK","items":{"email":{"username":"sgoyette9935@instasmail.com","timestamp":1624503547},"code":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InNnb3lldHRlOTkzNUBpbnN0YXNtYWlsLmNvbSIsInRpbWVzdGFtcCI6MTYyNDUwMzU0N30.oGs7oFoNEaR6gJ-n7T9_gsDZToiUpuE4hloA0Z8zOpg"}}
+        while (true) {
+            String result = HttpClientUtil.doPostJson("https://smailpro.com/app/email", param, header);
+            // {"code":200,"msg":"OK","items":{"email":{"username":"sgoyette9935@instasmail.com","timestamp":1624503547},"code":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InNnb3lldHRlOTkzNUBpbnN0YXNtYWlsLmNvbSIsInRpbWVzdGFtcCI6MTYyNDUwMzU0N30.oGs7oFoNEaR6gJ-n7T9_gsDZToiUpuE4hloA0Z8zOpg"}}
 //           String result = "{\"code\":200,\"msg\":\"OK\",\"items\":{\"email\":{\"username\":\"sgoyette9935@instasmail.com\",\"timestamp\":1624503547},\"code\":\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InNnb3lldHRlOTkzNUBpbnN0YXNtYWlsLmNvbSIsInRpbWVzdGFtcCI6MTYyNDUwMzU0N30.oGs7oFoNEaR6gJ-n7T9_gsDZToiUpuE4hloA0Z8zOpg\"}}";
-           System.out.println("result:" + result);
-           JSONObject jsonObject = JSON.parseObject(result);
-           if (jsonObject.getInteger("code").equals(200)) {
-               JSONObject emailJsonObj = jsonObject.getJSONObject("items").getJSONObject("email");
-               String email = emailJsonObj.getString("username");
-               if (email.endsWith("@gmail.com") && email.length() <= 32) {
-                   resultMap.put("email", email);
-                   resultMap.put("code", jsonObject.getJSONObject("items").getString("code"));
-                   return resultMap;
-               }
-           }
-           try {
-               Thread.sleep(50L);
-           } catch (InterruptedException e) {
-               e.printStackTrace();
-           }
-       }
+            System.out.println("result:" + result);
+            JSONObject jsonObject = JSON.parseObject(result);
+            if (jsonObject.getInteger("code").equals(200)) {
+                JSONObject emailJsonObj = jsonObject.getJSONObject("items").getJSONObject("email");
+                String email = emailJsonObj.getString("username");
+                if (email.endsWith("@gmail.com") && email.length() <= 32) {
+                    resultMap.put("email", email);
+                    resultMap.put("code", jsonObject.getJSONObject("items").getString("code"));
+                    return resultMap;
+                }
+            }
+            try {
+                Thread.sleep(50L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
@@ -83,15 +83,15 @@ public class TempEmailTest {
                     JSONObject jsonObject = resultJson.getJSONObject(0);
                     String msgStr = HttpClientUtil.doPostJson("https://smailpro.com/app/email/inbox/" + jsonObject.getString("mid"), param, header);
                     // {"code":200,"msg":"OK","items":"\u60a8\u672c\u6b21\u64cd\u4f5c\u7684\u9a8c\u8bc1\u7801\u4e3a <b>599746<\/b>\uff0c\u6709\u6548\u671f30\u5206\u949f\uff0c\u8bf7\u52ff\u6cc4\u9732\u7ed9\u4ed6\u4eba\uff0c\u5982\u975e\u672c\u4eba\u64cd\u4f5c\uff0c\u8bf7\u5ffd\u7565!"}
-                    String items = JSON.parseObject(msgStr).getString("items");
-                    String pattern = "\\d{4,10}";
+//                    String items = JSON.parseObject(msgStr).getString("items");
+                    String pattern = "<b>\\d{4,10}";
                     // 创建 Pattern 对象
                     Pattern r = Pattern.compile(pattern);
                     // 现在创建 matcher 对象
-                    Matcher m = r.matcher(items);
+                    Matcher m = r.matcher(msgStr);
                     if (m.find()) {
                         System.out.println("======================================================");
-                        System.out.println(email + ": " + m.group(0));
+                        System.out.println(email + ": " + m.group(0).substring(3));
                         System.out.println("======================================================");
                     }
                 }
